4ee81a40c298efd1504334eee32189db7d5849e2
SoyluEren2004 <139225815+SoyluEren2004@users.noreply.github.com>
2026-08-21 09:32:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include "RamCpuMonitor.h"
 #include "HardDiskMonitor.h"
-#include<iostream>
-#include <unistd.h>// sleep() ve usleep()
+#include "FanControl.h"
+#include <iostream>
+#include <unistd.h>
 
-struct 
-{
+struct {
     RamCpuMonitor ram_cpu_monitor;
     HardDiskMonitor hard_disk_monitor;
-}obj;
+    FanControl fan_control;
+} obj;
 
-
-int main(){
-    while(true){
-        system("clear");
+int main() {
+    while (true) {
         obj.ram_cpu_monitor.printInfo();
         obj.hard_disk_monitor.printDiskInfo();
-        sleep(3);
+        obj.fan_control.Fancontrol();
+        std::cout << "---FRAME END---" << std::endl;
+        sleep(5);
     }
-    
     return 0;
 }
